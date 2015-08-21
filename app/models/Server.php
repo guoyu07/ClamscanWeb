82bbec8e3524e6ce21ef4801c32b185dbd26cec4
Anthony Vitacco <anthony@littlegno.me>
2015-08-21 17:05:20 -0400

    Added getters and setters to Server model along with toArray and jsonSeralize (what a pain in the arse)

--- app/models/Server.php
+++ app/models/Server.php
@@ -7,7 +7,7 @@ Use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 /**
  * @ODM\Document(collection="servers")
  */
-class Server
+class Server implements \JsonSerializable
 {
     /** @ODM\Id */
     private $id;
@@ -64,5 +64,143 @@ class Server
     public function setName($value)
     {
         $this->name = $value;
+    }
+    
+    /**
+     *
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+    
+    /**
+     *
+     */
+    public function setAddress($value)
+    {
+        $this->address = $value;
+    }
+    
+    /**
+     *
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+    
+    /**
+     *
+     */
+    public function setPort($value)
+    {
+        $this->port = (int)$value;
+    }
+    
+    /**
+     *
+     */
+    public function getAuthMethod()
+    {
+        return $this->authMethod;
+    }
+    
+    /**
+     *
+     */
+    public function setAuthMethod($value)
+    {
+        $this->authMethod = $value;
+    }
+    
+    /**
+     *
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    
+    /**
+     *
+     */
+    public function setUsername($value)
+    {
+        $this->username = $value;
+    }
+    
+    /**
+     *
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    
+    /**
+     *
+     */
+    public function setPassword($value)
+    {
+        $this->password = $value;
+    }
+    
+    /**
+     *
+     */
+    public function getPublicKey()
+    {
+        return $this->publicKey;
+    }
+    
+    /**
+     *
+     */
+    public function setPublicKey($value)
+    {
+        $this->publicKey = $value;
+    }
+    
+    /**
+     *
+     */
+    public function getPrivateKey()
+    {
+        return $this->privateKey;
+    }
+    
+    /**
+     *
+     */
+    public function setPrivateKey($value)
+    {
+        $this->privateKey = $value;
+    }
+    
+    /**
+     *
+     */
+    public function toArray()
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "address" => $this->address,
+            "port" => $this->port,
+            "authMethod" => $this->authMethod,
+            "username" => $this->username,
+            "password" => $this->password,
+            "publicKey" => $this->publicKey,
+            "privateKey" => $this->privateKey,
+        ];
+    }
+    
+    /**
+     *
+     */
+    public function jsonSerialize()
+    {
+        return $this->toArray();
     }
 }
