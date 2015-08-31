9ee84e9f67f446ed4f488a592b50bbea628f2702
Anthony Vitacco <anthony@littlegno.me>
2015-08-31 15:20:29 -0400

    Modified all existing routes in the API controller to use match()->method() chain instead of get() or post() Added route definitions for server controller routes

--- app/routes/Api.php
+++ app/routes/Api.php
@@ -4,21 +4,8 @@
  * @license MIT
  */
 $app["controllers.pool"] = new \Iu\Uits\Webtech\ClamScanWeb\Controllers\Pool($app);
+$app["controllers.server"] = new \Iu\Uits\Webtech\ClamScanWeb\Controllers\Server($app);
 $api = $app["controllers_factory"];
-
-/**
- * GET
- * Return a list of available pools
- */
-$api->get("pools/list", "controllers.pool:getList")
-->bind("listPools");
-
-/**
- * GET
- * Get a specific pool
- */
-$api->get("pools/get/{poolId}", "controllers.pool:get")
-->bind("getPool");
 
 /**
  * PUT|POST
@@ -29,10 +16,26 @@ $api->match("pools/create", "controllers.pool:create")
 ->bind("createPool");
 
 /**
+ * GET
+ * Return a list of available pools
+ */
+$api->match("pools/list", "controllers.pool:getList")
+->method("GET")
+->bind("listPools");
+
+/**
+ * GET
+ * Get a specific pool
+ */
+$api->match("pools/get/{poolId}", "controllers.pool:get")
+->method("GET")
+->bind("getPool");
+
+/**
  * PATCH|POST
  * Update a pool
  */
-$api->match("/pools/update/{poolId}", "controllers.pool:update")
+$api->match("pools/update/{poolId}", "controllers.pool:update")
 ->method("PATCH|POST")
 ->bind("updatePool");
 
@@ -43,5 +46,52 @@ $api->match("/pools/update/{poolId}", "controllers.pool:update")
 $api->match("pools/delete/{poolId}", "controllers.pool:delete")
 ->method("DELETE|POST")
 ->bind("deletePool");
+
+/**
+ * GET
+ * Get the billboard for servers
+ */
+$api->match("servers", "controllers.server:billboard")
+->method("GET")
+->bind("serversBillboard");
+
+/**
+ * PUT|POST
+ * Create a new server
+ */
+$api->match("servers/create", "controllers.server:create")
+->method("PUT|POST")
+->bind("createServer");
+
+/**
+ * GET
+ * Get a list of available servers
+ */
+$api->match("servers/list", "controllers.server:getList")
+->method("GET")
+->bind("listServers");
+
+/**
+ * GET
+ * Get a specific server
+ */
+$api->match("servers/get/{serverId}", "controllers.server:get")
+->method("GET")
+->bind("getServer");
+
+/**
+ * PATCH|POST
+ */
+$api->match("servers/update/{serverId}", "controllers.server:update")
+->method("PATCH|POST")
+->bind("updateServer");
+
+/**
+ * DELETE|POST
+ * Delete an existing server
+ */
+$api->match("servers/delete/{serverId}", "controllers.server:delete")
+->method("DELETE|POST")
+->bind("deleteServer");
 
 $app->mount("/api", $api);
